9946fd357b93e3c2e2eb68598b30a9f48b10a0fe
Matt Wagner <mattgwagner@gmail.com>
2015-01-17 21:15:03 -0500

    Hook up some of the auth stuff

---
+++ BatteryCommander.Web/Models/AuthModels.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace BatteryCommander.Web.Models
+{
+    public class LoginModel
+    {
+        [Required]
+        [EmailAddress]
+        public String Username { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public String Password { get; set; }
+
+        [Display(Name = "Remember This Browser")]
+        public Boolean RememberMe { get; set; }
+
+        public String ReturnUrl { get; set; }
+    }
+}
