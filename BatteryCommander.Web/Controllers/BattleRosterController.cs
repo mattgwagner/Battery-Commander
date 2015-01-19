cbf25f9d89896a430c408d681a32578501a68928
Matt Wagner <mattgwagner@gmail.com>
2015-01-19 10:19:22 -0500

    Add empty BR controller

---
+++ BatteryCommander.Web/Controllers/BattleRosterController.cs
@@ -0,0 +1,29 @@
+﻿using BatteryCommander.Common;
+using BatteryCommander.Common.Models;
+using Microsoft.AspNet.Identity;
+using System.Data.Entity;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace BatteryCommander.Web.Controllers
+{
+    public class BattleRosterController : BaseController
+    {
+        private readonly DataContext _db;
+
+        public BattleRosterController(UserManager<AppUser, int> userManager, DataContext db)
+            : base(userManager)
+        {
+            _db = db;
+        }
+
+        public async Task<ActionResult> Index()
+        {
+            // TODO Which qualifications to show?
+
+            // TODO How to order soldiers?
+
+            return View();
+        }
+    }
+}
