e6ccfe23f17ad964b736b8598349f257dc4c730c
Matt Wagner <mattgwagner@gmail.com>
2015-01-22 16:58:50 -0500

    Bulk edit works

--- BatteryCommander.Web/Controllers/SoldierController.cs
+++ BatteryCommander.Web/Controllers/SoldierController.cs
@@ -2,7 +2,9 @@
 using BatteryCommander.Common.Models;
 using BatteryCommander.Web.Models;
 using Microsoft.AspNet.Identity;
+using System.Collections.Generic;
 using System.Data.Entity;
+using System.Linq;
 using System.Threading.Tasks;
 using System.Web.Mvc;
 
@@ -64,16 +66,56 @@ namespace BatteryCommander.Web.Controllers
             return View(model);
         }
 
+        [Route("Soldiers/Bulk")]
+        public async Task<ActionResult> Bulk()
+        {
+            var soldiers =
+                await _db
+                .Soldiers
+                .Select(s => new SoldierEditModel
+                {
+                    Id = s.Id,
+                    FirstName = s.FirstName,
+                    LastName = s.LastName,
+                    Rank = s.Rank,
+                    Status = s.Status
+                })
+                .ToListAsync();
+
+            return View(soldiers);
+        }
+
+        [Route("Soldiers/Bulk")]
+        [HttpPost, ValidateAntiForgeryToken]
+        public async Task<ActionResult> Bulk(IEnumerable<SoldierEditModel> models)
+        {
+            foreach (var model in models)
+            {
+                await AddOrUpdate(model);
+            }
+
+            return RedirectToAction("List");
+        }
+
         [Route("Soldier")]
         [HttpPost, ValidateAntiForgeryToken]
         public async Task<ActionResult> Save(SoldierEditModel model)
         {
             if (!ModelState.IsValid) return View("Edit", model);
 
+            var soldier = await AddOrUpdate(model);
+
+            return RedirectToAction("View", new { soldierId = soldier.Id });
+        }
+
+        private async Task<Soldier> AddOrUpdate(SoldierEditModel model)
+        {
             var soldier =
                 await _db
                 .Soldiers
                 .SingleOrDefaultAsync(s => s.Id == model.Id);
+
+            // TODO Check for duplicates by first/last name?
 
             if (soldier == null)
             {
@@ -87,7 +129,7 @@ namespace BatteryCommander.Web.Controllers
 
             await _db.SaveChangesAsync();
 
-            return RedirectToAction("View", new { soldierId = soldier.Id });
+            return soldier;
         }
     }
 }
