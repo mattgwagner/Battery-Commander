b233a67bba194c532af6ec85a76456a3dd972ecb
Matt Wagner <mattgwagner@gmail.com>
2015-02-02 17:02:42 -0500

    Provide display names for the education enum

--- BatteryCommander.Common/Models/MilitaryEducationLevel.cs
+++ BatteryCommander.Common/Models/MilitaryEducationLevel.cs
@@ -1,17 +1,27 @@
-﻿namespace BatteryCommander.Common.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BatteryCommander.Common.Models
 {
     public enum MilitaryEducationLevel
     {
         None,
 
+        [Display(Name = "(AIT) Advanced Individual Training")]
         AIT,
 
+        [Display(Name = "(WLC) Warrior Leader Course")]
         WLC,
 
+        [Display(Name = "Advanced Leader Course")]
+        ALC,
+
+        [Display(Name = "Senior Leader Course")]
         SLC,
 
+        [Display(Name = "Basic Officer Leadership Course")]
         BOLC,
 
+        [Display(Name = "Captains Career Course")]
         CCC
     }
 }
