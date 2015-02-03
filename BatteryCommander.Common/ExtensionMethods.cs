2463e3cde1df78e72fa06d1dfd85a8f055760762
Matt Wagner <mattgwagner@gmail.com>
2015-02-03 16:28:03 -0500

    Add an extension method for display name enum

---
+++ BatteryCommander.Common/ExtensionMethods.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel;
+using System.Reflection;
+
+namespace BatteryCommander.Common
+{
+    public static class ExtensionMethods
+    {
+        public static string DisplayName(this Enum val)
+        {
+            FieldInfo fi = val.GetType().GetField(val.ToString());
+
+            DescriptionAttribute[] attributes = (DescriptionAttribute[])fi.GetCustomAttributes(typeof(DescriptionAttribute), false);
+
+            if (attributes != null && attributes.Length > 0)
+            {
+                return attributes[0].Description;
+            }
+
+            return val.ToString();
+        }
+    }
+}
