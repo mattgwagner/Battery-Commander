b102e90505b0a55aa778fbfefc0d69901e93451d
Matt Wagner <mattgwagner@gmail.com>
2015-02-05 11:15:43 -0500

    Add version # to the bottom

---
+++ BatteryCommander.Common/Utilities.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+
+namespace BatteryCommander.Common
+{
+    public class Utilities
+    {
+        private static Lazy<String> version = new Lazy<String>(() => Assembly.GetExecutingAssembly().GetCustomAttributes(false).OfType<AssemblyInformationalVersionAttribute>().Single().InformationalVersion);
+
+        /// <summary>
+        /// Returns the AssemblyFileVersion attribute of the library, or what version of the code is running
+        /// </summary>
+        public static String Version { get { return version.Value; } }
+    }
+}
