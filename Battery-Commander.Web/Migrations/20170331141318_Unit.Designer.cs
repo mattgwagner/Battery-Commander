435640ddd8f9a0d8a255f13214de3d07ad427ffd
Matt Wagner <mattgwagner@gmail.com>
2017-03-31 10:14:15 -0400

    Add unit model and soldier relation

--- Battery-Commander.Web/Migrations/20170331141318_Unit.Designer.cs
+++ Battery-Commander.Web/Migrations/20170331141318_Unit.Designer.cs
@@ -8,9 +8,10 @@ using BatteryCommander.Web.Models;
 namespace BatteryCommander.Web.Migrations
 {
     [DbContext(typeof(Database))]
-    partial class DatabaseModelSnapshot : ModelSnapshot
+    [Migration("20170331141318_Unit")]
+    partial class Unit
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
             modelBuilder
                 .HasAnnotation("ProductVersion", "1.1.1");
