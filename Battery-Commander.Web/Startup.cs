102f95f70d3e6cc55058defa7f6752a7ba51b6f6
Matt Wagner <mattgwagner@gmail.com>
2017-03-30 21:52:08 -0400

    Need to register the db

--- Battery-Commander.Web/Startup.cs
+++ Battery-Commander.Web/Startup.cs
@@ -51,6 +51,8 @@ namespace BatteryCommander.Web
 
             // Add the Auth0 Settings object so it can be injected
             services.Configure<Auth0Settings>(Configuration.GetSection("Auth0"));
+
+            services.AddDbContext<Database>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
