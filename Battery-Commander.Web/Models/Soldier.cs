86f90295bc5c8da55534b705268ace90b74cb155
Matt Wagner <mattgwagner@gmail.com>
2017-04-07 12:14:08 -0400

    Change print naming for SM

--- Battery-Commander.Web/Models/Soldier.cs
+++ Battery-Commander.Web/Models/Soldier.cs
@@ -75,6 +75,6 @@ namespace BatteryCommander.Web.Models
 
         public virtual ICollection<ABCP> ABCPs { get; set; }
 
-        public override string ToString() => $"{Rank} {LastName}, {FirstName} {MiddleName}";
+        public override string ToString() => $"{LastName} {FirstName} {MiddleName}".ToUpper();
     }
 }
