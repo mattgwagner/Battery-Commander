861b63bc33e29cb35b76008e7b5f283772f7f6c6
Matt Wagner <mattgwagner@gmail.com>
2017-04-13 09:44:14 -0400

    Pacakge is local, not in output

--- Package.ps1
+++ Package.ps1
@@ -6,12 +6,14 @@ $ErrorActionPreference = "Stop"
 
 $Here = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
+$7Zip = "${env:ProgramFiles}\7-Zip\7z.exe"
+
 $DotNet = "${env:ProgramFiles}\dotnet\dotnet.exe"
 
 $Website = Join-Path $Here "Battery-Commander.Web\Battery-Commander.Web.csproj"
 
 & $DotNet publish $Website --configuration $Configuration --output "$Here/output"
 
-7z a BatteryCommander.Web.zip "$Here/output"
+& $7Zip a BatteryCommander.Web.zip "$Here/output/**"
 
 EXIT $LASTEXITCODE
