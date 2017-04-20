f8f86b7084e30e70a8411824e3833de25df1a16c
Matt Wagner <mattgwagner@gmail.com>
2017-04-19 21:34:19 -0400

    Forgot the import

--- Battery-Commander.Web/Controllers/UnitsController.cs
+++ Battery-Commander.Web/Controllers/UnitsController.cs
@@ -1,4 +1,5 @@
 ﻿using BatteryCommander.Web.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
