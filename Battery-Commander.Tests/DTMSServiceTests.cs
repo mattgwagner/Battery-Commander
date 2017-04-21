db366b17ca66524205a504c3c275d991fc2cf077
Matt Wagner <mattgwagner@gmail.com>
2017-04-20 23:02:19 -0400

    Trying to stub out a test EF db

--- Battery-Commander.Tests/DTMSServiceTests.cs
+++ Battery-Commander.Tests/DTMSServiceTests.cs
@@ -1,7 +1,4 @@
-﻿using BatteryCommander.Web.Models;
-using BatteryCommander.Web.Services;
-using Microsoft.AspNetCore.Http;
-using Moq;
+﻿using BatteryCommander.Web.Services;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
@@ -14,15 +11,9 @@ namespace BatteryCommander.Tests
         [Fact]
         public async Task Parse_Import()
         {
-            var db = new Mock<Database>();
-
             using (var file = new FileStream("Scrubbed.xlsx", FileMode.Open))
             {
-                var fileinfo = new Mock<IFormFile>();
-
-                fileinfo.Setup(_ => _.OpenReadStream()).Returns(file);
-
-                var soldiers = await DTMSService.ImportSoldiers(db.Object, fileinfo.Object);
+                var soldiers = await DTMSService.ImportSoldiers(new FakeDb { }, file);
 
                 Assert.Equal(9, soldiers.Count());
             }
---
+++ Battery-Commander.Tests/FakeDatabase.cs
@@ -0,0 +1,18 @@
+﻿using BatteryCommander.Web.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BatteryCommander.Tests
+{
+    public class FakeDb : Database
+    {
+        public FakeDb()
+        {
+            BatteryCommander.Web.Models.Database.Init(this);
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder builder)
+        {
+            builder.UseSqlite("Data Source=:memory:");
+        }
+    }
+}
