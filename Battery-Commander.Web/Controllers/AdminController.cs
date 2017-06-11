7b7b4530f5f9c33d416e770a3cf8f4a9706fe32c
Matt Wagner <mattgwagner@gmail.com>
2017-06-11 10:55:57 -0400

    Start splitting of admin functionality

---
+++ Battery-Commander.Web/Controllers/AdminController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace BatteryCommander.Web.Controllers
+{
+    [Authorize]
+    public class AdminController : Controller
+    {
+        // Admin Tasks:
+
+        // Add/Remove Users
+
+        // Backup SQLite Db
+
+        // Scrub Soldier Data
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> Scrub()
+        {
+            // Go through each soldier and fix casing on their name
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Backup()
+        {
+            var data = System.IO.File.ReadAllBytes("Data.db");
+
+            var mimeType = "application/octet-stream";
+
+            return File(data, mimeType);
+        }
+    }
+}
--- Battery-Commander.Web/Controllers/HomeController.cs
+++ Battery-Commander.Web/Controllers/HomeController.cs
@@ -92,15 +92,6 @@ namespace BatteryCommander.Web.Controllers
             await HttpContext.Authentication.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
         }
 
-        public IActionResult Backup()
-        {
-            var data = System.IO.File.ReadAllBytes("Data.db");
-
-            var mimeType = "application/octet-stream";
-
-            return File(data, mimeType);
-        }
-
         public IActionResult Error()
         {
             return View();
