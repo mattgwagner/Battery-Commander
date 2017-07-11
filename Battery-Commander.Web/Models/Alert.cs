7f577ce0df03b6d9d47c462e14eaf6b854789bc6
Matt Wagner <mattgwagner@gmail.com>
2017-07-10 22:37:54 -0400

    Add notes on the alert model

---
+++ Battery-Commander.Web/Models/Alert.cs
@@ -0,0 +1,23 @@
+﻿namespace BatteryCommander.Web.Models
+{
+    public class Alert
+    {
+        // Id
+
+        // SentAt
+
+        // Recipients - IDs? - Pull Phone, Email from Soldier entity
+
+        // Format - SMS? Email? Phone?
+
+        // Message / Content
+
+        // Status - Open, Closed -- Only one open at a time, all responses will be attributed to that alert
+        // Responses - FROM, CONTENT
+
+        // Request Ack T/F?
+        // What about sending a short-URL to acknowledge receipt? https://abcd.red-leg-dev.com/Alert/{id}/Acknowledge
+        // User enters name/identifying info, accessible from phones, can provide more context
+        // OR, each user gets a custom URL based on receipient hash so that we don't even need more than a big ACK button?
+    }
+}
