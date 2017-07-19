8b7155060e3c00511fb9fa26f1e0043238fe9f67
Matt Wagner <mattgwagner@gmail.com>
2017-07-19 10:32:14 -0400

    Need to tweak the updated humanized

--- Battery-Commander.Web/Models/SSD.cs
+++ Battery-Commander.Web/Models/SSD.cs
@@ -136,7 +136,7 @@ namespace BatteryCommander.Web.Models
             public TimeSpan? Updated => (DateTimeOffset.UtcNow - AsOf);
 
             [Display(Name = "Updated")]
-            public String UpdatedHumanized => Updated?.Humanize(minUnit: Humanizer.Localisation.TimeUnit.Day);
+            public String UpdatedHumanized => Updated?.Humanize();
 
             // Humanized time since
 
