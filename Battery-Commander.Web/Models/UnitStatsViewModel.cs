a41989f6cd43bc1d4d73fe454bd81c3cf68c1a06
Matt Wagner <mattgwagner@gmail.com>
2017-07-20 16:33:54 -0400

    Build model fro SSD status dashboard

--- Battery-Commander.Web/Models/UnitStatsViewModel.cs
+++ Battery-Commander.Web/Models/UnitStatsViewModel.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using static BatteryCommander.Web.Models.Soldier;
 
 namespace BatteryCommander.Web.Models
 {
@@ -12,6 +14,20 @@ namespace BatteryCommander.Web.Models
         public Stat ABCP { get; set; } = new Stat { };
 
         public Stat APFT { get; set; } = new Stat { };
+
+        public ICollection<SSDStat> SSD { get; set; } = new List<SSDStat>();
+
+        public class SSDStat
+        {
+            public Rank Rank { get; set; }
+
+            public int Assigned { get; set; }
+
+            public int Completed { get; set; }
+
+            [DisplayFormat(DataFormatString = SSDStatusModel.Format)]
+            public Decimal Percentage => Assigned > 0 ? Completed / Assigned : Decimal.Zero;
+        }
 
         public class Stat
         {
