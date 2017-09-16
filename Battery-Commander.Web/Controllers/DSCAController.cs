09c7fd84877a5da6b696485985f54e70151afbb3
Matt Wagner <mattgwagner@gmail.com>
2017-09-15 21:34:27 -0400

    Template for list of dsca and iwq updates

---
+++ Battery-Commander.Web/Controllers/DSCAController.cs
@@ -0,0 +1,25 @@
+﻿using BatteryCommander.Web.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BatteryCommander.Web.Controllers
+{
+    [Authorize]
+    public class DSCAController : Controller
+    {
+        private readonly Database db;
+
+        public DSCAController(Database db)
+        {
+            this.db = db;
+        }
+
+        // List status for all soldiers by - filter by unit
+
+        // Bulk update qual status/date
+    }
+}
