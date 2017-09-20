b461abc97fce2aeb287625190788c6c48e1ffc6e
Matt Wagner <mattgwagner@gmail.com>
2017-09-20 14:31:20 -0400

    Migration to IWQ data

---
+++ Battery-Commander.Web/Migrations/20170920183001_IWQ.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BatteryCommander.Web.Migrations
+{
+    public partial class IWQ : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "IwqQualificationDate",
+                table: "Soldiers",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IwqQualificationDate",
+                table: "Soldiers");
+        }
+    }
+}
