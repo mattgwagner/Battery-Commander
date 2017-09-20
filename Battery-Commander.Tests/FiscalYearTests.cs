6d6975ca3a61f10c53a0c01bb56947804b44ff17
Matt Wagner <mattgwagner@gmail.com>
2017-09-20 11:02:59 -0400

    Add tests for FY handling

--- Battery-Commander.Tests/FiscalYearTests.cs
+++ Battery-Commander.Tests/FiscalYearTests.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using Xunit;
+using BatteryCommander.Web.Services;
 
 namespace BatteryCommander.Tests
 {
@@ -11,7 +12,19 @@ namespace BatteryCommander.Tests
 
         public void Check_FY_2017_Start()
         {
+            Assert.Equal(new DateTime(2016, 10, 1), FiscalYear.FY2017.Start());
+        }
 
+        [Fact]
+        public void Check_FY_2017_Next()
+        {
+            Assert.Equal(FiscalYear.FY2018, FiscalYear.FY2017.Next());
+        }
+
+        [Fact]
+        public void Check_FY_2017_End()
+        {
+            Assert.Equal(new DateTime(2017, 9, 30), FiscalYear.FY2017.End());
         }
     }
 }
