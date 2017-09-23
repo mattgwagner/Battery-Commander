a4627eb8b28ceddef0efee87d161fca4c779d2a0
Matt Wagner <mattgwagner@gmail.com>
2017-09-22 21:58:53 -0400

    Add base for report generation

---
+++ Battery-Commander.Web/Controllers/ReportsController.cs
@@ -0,0 +1,51 @@
+﻿using BatteryCommander.Web.Models;
+using BatteryCommander.Web.Models.Reports;
+using BatteryCommander.Web.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace BatteryCommander.Web.Controllers
+{
+    [Authorize]
+    public class ReportsController : Controller
+    {
+        private readonly Database db;
+
+        public ReportsController(Database db)
+        {
+            this.db = db;
+        }
+
+        // Generate HTML/PDF version
+        // Email to configurable address on request / on schedule
+
+        // GREEN 3 -- Sensitive Items
+
+        // CONVOY MANIFEST?
+
+        // TAN 1 -- Comstat
+
+        // YELLOW 1 -- LOGSTAT
+
+        public async Task<IActionResult> Red1(SoldierSearchService.Query query)
+        {
+            var model = new Red1_Perstat
+            {
+                Soldiers = await SoldierSearchService.Filter(db, query)
+            };
+
+            return Json(model);
+        }
+
+        public async Task<IActionResult> SadPerstat(SoldierSearchService.Query query)
+        {
+            var model = new StateActiveDuty_Perstat
+            {
+                Soldiers = await SoldierSearchService.Filter(db, query)
+            };
+
+            return Json(model);
+        }
+    }
+}
