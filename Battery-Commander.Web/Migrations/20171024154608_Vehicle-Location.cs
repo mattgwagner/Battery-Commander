5bd64077fc3f1089d07b29599b178a708d3f63bb
Matt Wagner <mattgwagner@gmail.com>
2017-10-24 11:47:05 -0400

    Add migration for vehicle location

---
+++ Battery-Commander.Web/Migrations/20171024154608_Vehicle-Location.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BatteryCommander.Web.Migrations
+{
+    public partial class VehicleLocation : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<byte>(
+                name: "Location",
+                table: "Vehicles",
+                nullable: false,
+                defaultValue: Models.Vehicle.VehicleLocation.HS);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Location",
+                table: "Vehicles");
+        }
+    }
+}
