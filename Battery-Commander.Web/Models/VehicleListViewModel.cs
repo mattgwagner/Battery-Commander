d4a32ba093f8044d3b7ddc067247f541c53d7512
Matt Wagner <mattgwagner@gmail.com>
2017-10-24 22:35:45 -0400

    Adjust vehicle list to only include FMC and at HS

--- Battery-Commander.Web/Models/VehicleListViewModel.cs
+++ Battery-Commander.Web/Models/VehicleListViewModel.cs
@@ -10,12 +10,12 @@ namespace BatteryCommander.Web.Models
 
         public IEnumerable<Vehicle> Vehicles { get; set; } = Enumerable.Empty<Vehicle>();
 
-        public int FMC => Vehicles.Where(_ => _.FMC).Count();
+        protected IEnumerable<Vehicle> Available => Vehicles.Where(_ => _.FMC).Where(_ => _.Location == Vehicle.VehicleLocation.HS);
 
-        public int OnHand => Vehicles.Where(_ => _.Status != Vehicle.VehicleStatus.Unknown).Count();
+        public int FMC => Available.Count();
 
-        public int PAX => Vehicles.Where(_ => _.FMC).Where(_ => _.DriverId.HasValue).Count() + Vehicles.Where(_ => _.FMC).Where(_ => _.A_DriverId.HasValue).Count();
+        public int PAX => Available.Where(_ => _.DriverId.HasValue).Count() + Available.Where(_ => _.FMC).Where(_ => _.A_DriverId.HasValue).Count();
 
-        public int Seats => Vehicles.Where(_ => _.FMC).Select(_ => _.Seats).Sum();
+        public int Seats => Available.Select(_ => _.Seats).Sum();
     }
 }
