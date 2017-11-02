aa820877232e158e4122dabc20138825d0dc1481
Matt Wagner <mattgwagner@gmail.com>
2017-11-01 22:00:31 -0400

    Make ABCP match APFT header

---
+++ Battery-Commander.Web/Models/ABCPListViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace BatteryCommander.Web.Models
+{
+    public class ABCPListViewModel
+    {
+        public IEnumerable<Soldier> Soldiers { get; set; } = Enumerable.Empty<Soldier>();
+
+        public int Go => Soldiers.Where(_ => _.ABCPs.Any()).Where(_ => _.LastAbcp.IsPassing).Count();
+
+        public int NoGo => Soldiers.Where(_ => _.ABCPs.Any()).Where(_ => !_.LastAbcp.IsPassing).Count();
+
+        public int Due => Soldiers.Where(_ => !_.ABCPs.Any() || !_.LastAbcp.IsValid).Count();
+    }
+}
