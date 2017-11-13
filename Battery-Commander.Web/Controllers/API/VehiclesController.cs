bcbe3a4c5a8e86ce0c9d4c51dd1ed5c7a88cd2a9
Matt Wagner <mattgwagner@gmail.com>
2017-11-12 20:40:01 -0500

    Add stub endpoint for getting vehicle info

---
+++ Battery-Commander.Web/Controllers/API/VehiclesController.cs
@@ -0,0 +1,47 @@
+﻿using BatteryCommander.Web.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BatteryCommander.Web.Controllers.API
+{
+    [Route("api/[controller]"), Authorize]
+    public class VehiclesController : Controller
+    {
+        private readonly Database db;
+
+        public VehiclesController(Database db)
+        {
+            this.db = db;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<dynamic>> Get()
+        {
+            // GET: api/vehicles
+
+            return
+                await db
+                .Vehicles
+                .Select(_ => new
+                {
+                    _.Id,
+                    _.UnitId,
+                    _.Bumper,
+                    _.Registration,
+                    _.Nomenclature,
+                    _.Notes,
+                    _.Serial,
+                    _.Status,
+                    _.Type,
+                    _.HasFuelCard,
+                    _.HasJBCP,
+                    _.HasTowBar
+                })
+                .ToListAsync();
+        }
+    }
+}
