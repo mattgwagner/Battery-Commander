08381acd81916da0c3a6fe3f2d08e87c9a7340b7
Matt Wagner <mattgwagner@gmail.com>
2017-11-24 20:35:31 -0500

    Add component for nav header

---
+++ Battery-Commander.Web/Models/NavigationViewComponent.cs
@@ -0,0 +1,22 @@
+﻿using BatteryCommander.Web.Services;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace BatteryCommander.Web.Models
+{
+    public class NavigationViewComponent : ViewComponent
+    {
+        private readonly Database db;
+
+        public NavigationViewComponent(Database db)
+        {
+            this.db = db;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync(ClaimsPrincipal user)
+        {
+            return View(await UserService.FindAsync(db, user));
+        }
+    }
+}
