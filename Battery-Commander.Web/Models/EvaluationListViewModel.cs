699929a3ec5f32f4ae3b5574cc201cb62de5766d
Matt Wagner <mattgwagner@gmail.com>
2018-01-06 22:18:42 -0500

    Make sure to take into account completed when looking at ALL

--- Battery-Commander.Web/Models/EvaluationListViewModel.cs
+++ Battery-Commander.Web/Models/EvaluationListViewModel.cs
@@ -10,17 +10,17 @@ namespace BatteryCommander.Web.Models
         public IEnumerable<Evaluation> Evaluations { get; set; } = Enumerable.Empty<Evaluation>();
 
         [Display(Name = "Delinquent > 60 Days")]
-        public int Delinquent => Evaluations.Where(_ => _.IsDelinquent).Count();
+        public int Delinquent => Evaluations.Where(_ => !_.IsCompleted).Where(_ => _.IsDelinquent).Count();
 
-        public int Due => Evaluations.Where(_ => !_.IsDelinquent).Where(_ => _.ThruDate < DateTime.Today).Count();
+        public int Due => Evaluations.Where(_ => !_.IsCompleted).Where(_ => !_.IsDelinquent).Where(_ => _.ThruDate < DateTime.Today).Count();
 
         [Display(Name = "Next 30")]
-        public int Next30 => Evaluations.Where(_ => 0 <= _.Delinquency.TotalDays && _.Delinquency.TotalDays <= 30).Count();
+        public int Next30 => Evaluations.Where(_ => !_.IsCompleted).Where(_ => 0 <= _.Delinquency.TotalDays && _.Delinquency.TotalDays <= 30).Count();
 
         [Display(Name = "Next 60")]
-        public int Next60 => Evaluations.Where(_ => 30 < _.Delinquency.TotalDays && _.Delinquency.TotalDays <= 60).Count();
+        public int Next60 => Evaluations.Where(_ => !_.IsCompleted).Where(_ => 30 < _.Delinquency.TotalDays && _.Delinquency.TotalDays <= 60).Count();
 
         [Display(Name = "Next 90")]
-        public int Next90 => Evaluations.Where(_ => 60 < _.Delinquency.TotalDays && _.Delinquency.TotalDays <= 90).Count();
+        public int Next90 => Evaluations.Where(_ => !_.IsCompleted).Where(_ => 60 < _.Delinquency.TotalDays && _.Delinquency.TotalDays <= 90).Count();
     }
 }
