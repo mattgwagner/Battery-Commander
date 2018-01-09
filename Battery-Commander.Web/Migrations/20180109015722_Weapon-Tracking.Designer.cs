f90b014c8d56745a3da7a1732d5816a7815d63a1
Matt Wagner <mattgwagner@gmail.com>
2018-01-08 21:10:46 -0500

    Missed namespace on db

--- Battery-Commander.Web/Migrations/20180109015722_Weapon-Tracking.Designer.cs
+++ Battery-Commander.Web/Migrations/20180109015722_Weapon-Tracking.Designer.cs
@@ -10,7 +10,7 @@ using System;
 
 namespace BatteryCommander.Web.Migrations
 {
-    [DbContext(typeof(Database))]
+    [DbContext(typeof(Models.Database))]
     [Migration("20180109015722_Weapon-Tracking")]
     partial class WeaponTracking
     {
