ba1d839d617f3e40b4030bf7edc17112e5ba4aca
Matt Wagner <mattgwagner@gmail.com>
2018-01-08 21:21:19 -0500

    Show count type by type at top

--- Battery-Commander.Web/Models/WeaponListViewModel.cs
+++ Battery-Commander.Web/Models/WeaponListViewModel.cs
@@ -6,5 +6,16 @@ namespace BatteryCommander.Web.Models
     public class WeaponListViewModel
     {
         public IEnumerable<Weapon> Weapons { get; set; } = Enumerable.Empty<Weapon>();
+
+        public IEnumerable<KeyValuePair<Weapon.WeaponType, int>> Count_By_Type
+        {
+            get
+            {
+                return
+                    Weapons
+                    .GroupBy(weapon => weapon.Type)
+                    .Select(grouped => KeyValuePair.Create(grouped.Key, grouped.Count()));
+            }
+        }
     }
 }
