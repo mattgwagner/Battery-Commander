4f3785552339db8fa4baa87bb90dff617ef70091
Matt Wagner <mattgwagner@gmail.com>
2018-01-08 21:01:28 -0500

    Starting to stub out controller endpoints

---
+++ Battery-Commander.Web/Controllers/WeaponsController.cs
@@ -0,0 +1,84 @@
+﻿using BatteryCommander.Web.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BatteryCommander.Web.Controllers
+{
+    [Authorize, ApiExplorerSettings(IgnoreApi = true)]
+    public class WeaponsController : Controller
+    {
+        private readonly Database db;
+
+        public WeaponsController(Database db)
+        {
+            this.db = db;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            // List of Weapons - by unit, by status
+
+            return View("List", new WeaponListViewModel
+            {
+                Weapons =
+                    await db
+                    .Weapons
+                    .ToListAsync()
+            });
+        }
+
+        public async Task<IActionResult> New()
+        {
+            ViewBag.Soldiers = await SoldiersController.GetDropDownList(db, includeIgnoredUnits: false);
+            ViewBag.Units = await UnitsController.GetDropDownList(db);
+
+            return View(nameof(Edit), new Weapon { });
+        }
+
+        public async Task<IActionResult> Edit(int id)
+        {
+            ViewBag.Soldiers = await SoldiersController.GetDropDownList(db, includeIgnoredUnits: false);
+            ViewBag.Units = await UnitsController.GetDropDownList(db);
+
+            var model =
+                await db
+                .Weapons
+                .Where(_ => _.Id == id)
+                .SingleOrDefaultAsync();
+
+            return View(model);
+        }
+
+        [HttpPost, ValidateAntiForgeryToken]
+        public async Task<IActionResult> Save(Weapon model)
+        {
+            if (await db.Weapons.AnyAsync(weapon => weapon.Id == model.Id) == false)
+            {
+                db.Weapons.Add(model);
+            }
+            else
+            {
+                db.Weapons.Update(model);
+            }
+
+            await db.SaveChangesAsync();
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpPost, ValidateAntiForgeryToken]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var weapon = await db.Weapons.SingleAsync(_ => _.Id == id);
+
+            db.Weapons.Remove(weapon);
+
+            await db.SaveChangesAsync();
+
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
