8ebd087245ce4a2b15152cf07213a9272f1c3333
Matt Wagner <mattgwagner@gmail.com>
2018-01-19 20:22:22 -0500

    Fix display attribute compile

--- Battery-Commander.Web/Models/ABCP.cs
+++ Battery-Commander.Web/Models/ABCP.cs
@@ -214,7 +214,7 @@ namespace BatteryCommander.Web.Models
         /// <summary>
         /// Returns true if the Soldier made satisfactory progress since the last ABCP weigh-in
         /// </summary>
-        [NotMapped, Display("Is Satisfactory Progress?")]
+        [NotMapped, Display(Name = "Is Satisfactory Progress?")]
         public Boolean IsSatisfactory
         {
             get
