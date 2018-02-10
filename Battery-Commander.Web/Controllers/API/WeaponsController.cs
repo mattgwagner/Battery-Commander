b333ebca4dd2618c41a7f1816136b4406e5b1c57
Matt Wagner <mattgwagner@gmail.com>
2018-02-10 17:02:15 -0500

    Add simple controller for weapons

---
+++ Battery-Commander.Web/Controllers/API/WeaponsController.cs
@@ -0,0 +1,53 @@
+﻿using BatteryCommander.Web.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace BatteryCommander.Web.Controllers.API
+{
+    [Route("api/[controller]"), Authorize]
+    public class WeaponsController : Controller
+    {
+        private readonly Database db;
+
+        public WeaponsController(Database db)
+        {
+            this.db = db;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<dynamic>> Get()
+        {
+            // GET: api/weapons
+
+            return
+                await db
+                .Weapons
+                .Select(_ => new
+                {
+                    _.Id,
+                    _.OpticSerial,
+                    _.OpticType,
+                    _.Serial,
+                    _.StockNumber,
+                    _.Type,
+                    _.UnitId
+                })
+                .ToListAsync();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post(Weapon weapon)
+        {
+            await db.Weapons.AddAsync(weapon);
+
+            await db.SaveChangesAsync();
+
+            return Ok();
+        }
+    }
+}
