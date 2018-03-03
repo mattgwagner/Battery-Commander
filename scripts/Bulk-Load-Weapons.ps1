20cc4f7122406a3c8fffd328e29876b87d6bd52d
Matt Wagner <mattgwagner@gmail.com>
2018-03-03 14:04:47 -0500

    Continued work on weapon bulk load

--- scripts/Bulk-Load-Weapons.ps1
+++ scripts/Bulk-Load-Weapons.ps1
@@ -2,13 +2,25 @@ $Here = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 Import-Module "$Here\_Helpers" -DisableNameChecking
 
+# Clear out existing -- this is not what we're looking for if we've got multiple units
+
+(BC-Get -Uri "$URL/weapons")
+
 foreach($Weapon in (BC-Get -Uri "$URL/weapons"))
 {
 	Write-Output "Removing Weapon with ID $($Weapon.Id)"
 
-	Invoke-RestMethod -Method Delete -Uri "$URL/weapons/$($Weapon.Id)" -Headers $Headers
+	#BC-Delete -Uri "$URL/weapons/$($Weapon.Id)"
 }
 
 # Get weapons from CSV file
 
-# Create new weapon with info
+foreach($Weapon in (Import-Csv -Path $Here\WEAPONS_ROSTER.csv))
+{
+	# Create new weapon with info
+
+	#Write-Output "Create new MAL entry for $Weapon"
+
+	#BC-Post -Uri "$URL/Weapons" -Body @{ UnitId = 3; AdminNumber = $Weapon.NUM; Serial = $Weapon.SERIAL; OpticSerial = $Weapon.CCO }
+}
+
