9e9b51bdd435142718da4db5290e7edab508ca7d
Matt Wagner <mattgwagner@gmail.com>
2018-04-26 20:50:35 -0400

    Fix namespaces

--- Battery-Commander.Web/Migrations/20180427004924_TroopCapacity.Designer.cs
+++ Battery-Commander.Web/Migrations/20180427004924_TroopCapacity.Designer.cs
@@ -10,7 +10,7 @@ using System;
 
 namespace BatteryCommander.Web.Migrations
 {
-    [DbContext(typeof(Database))]
+    [DbContext(typeof(Models.Database))]
     [Migration("20180427004924_TroopCapacity")]
     partial class TroopCapacity
     {
