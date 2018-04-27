f8105dad2e080f9a870d40f6b4cbbe708800563e
Matt Wagner <mattgwagner@gmail.com>
2018-04-26 21:13:47 -0400

    Add migration for linking to passengers of vehicles

---
+++ Battery-Commander.Web/Migrations/20180427011333_Passengers.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace BatteryCommander.Web.Migrations
+{
+    public partial class Passengers : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Passengers",
+                columns: table => new
+                {
+                    VehicleId = table.Column<int>(nullable: false),
+                    SoldierId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Passengers", x => new { x.VehicleId, x.SoldierId });
+                    table.ForeignKey(
+                        name: "FK_Passengers_Soldiers_SoldierId",
+                        column: x => x.SoldierId,
+                        principalTable: "Soldiers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Passengers_Vehicles_VehicleId",
+                        column: x => x.VehicleId,
+                        principalTable: "Vehicles",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Passengers_SoldierId",
+                table: "Passengers",
+                column: "SoldierId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Passengers");
+        }
+    }
+}
