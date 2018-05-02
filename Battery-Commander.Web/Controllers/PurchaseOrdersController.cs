2a4b4178927935d298423b65383f4440754cf1e9
Matt Wagner <mattgwagner@gmail.com>
2018-05-02 12:55:47 -0400

    Add controller endpoint to allow adding purchase orders for AirTable

---
+++ Battery-Commander.Web/Controllers/PurchaseOrdersController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BatteryCommander.Web.Controllers
+{
+    public class PurchaseOrdersController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [AllowAnonymous]
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
