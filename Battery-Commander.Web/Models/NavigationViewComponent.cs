824ebc061643adaf70b71183fe900a2c0c4e3b8c
Matt Wagner <mattgwagner@gmail.com>
2018-06-07 21:47:58 -0400

    Don't show SUTA/PO tracker unless you're me, for now

--- Battery-Commander.Web/Models/NavigationViewComponent.cs
+++ Battery-Commander.Web/Models/NavigationViewComponent.cs
@@ -14,6 +14,12 @@ namespace BatteryCommander.Web.Models
 
         public Boolean ShowNavigation => !String.Equals(nameof(HomeController.PrivacyAct), RouteData.Values["action"]);
 
+        // TODO Fix this to actually check a feature flag
+
+        public Boolean ShowSUTATracker => UserClaimsPrincipal?.Identity.Name == "mattgwagner@gmail.com";
+
+        public Boolean ShowPOTracker => UserClaimsPrincipal?.Identity.Name == "mattgwagner@gmail.com";
+
         public NavigationViewComponent(Database db)
         {
             this.db = db;
