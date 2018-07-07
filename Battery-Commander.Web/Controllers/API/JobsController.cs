43ed52bbf6a49c57aeb1dc51df05c07dc20cd914
mattgwagner <MattGWagner@gmail.com>
2018-07-06 23:32:51 -0400

    Add endpoints to view jobs

---
+++ Battery-Commander.Web/Controllers/API/JobsController.cs
@@ -0,0 +1,30 @@
+﻿using BatteryCommander.Web.Models;
+using FluentScheduler;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+
+namespace BatteryCommander.Web.Controllers.API
+{
+    public class JobsController : ApiController
+    {
+        public JobsController(Database db) : base(db)
+        {
+            // Nothing to do here
+        }
+
+        [HttpGet]
+        public IEnumerable<Schedule> List()
+        {
+            return JobManager.AllSchedules;
+        }
+
+        [HttpPost("{name}")]
+        public IActionResult Post(String name)
+        {
+            JobManager.GetSchedule(name)?.Execute();
+
+            return Ok();
+        }
+    }
+}
