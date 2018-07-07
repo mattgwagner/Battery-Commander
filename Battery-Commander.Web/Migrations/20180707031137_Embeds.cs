5f35eeddd6c9c7e3f68540d918c4a1d1ed4d3905
mattgwagner <MattGWagner@gmail.com>
2018-07-06 23:13:56 -0400

    Rescaffold the db

---
+++ Battery-Commander.Web/Migrations/20180707031137_Embeds.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace BatteryCommander.Web.Migrations
+{
+    public partial class Embeds : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Embeds",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(maxLength: 20, nullable: true),
+                    Route = table.Column<string>(maxLength: 30, nullable: true),
+                    Source = table.Column<string>(nullable: true),
+                    UnitId = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Embeds", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Embeds_Units_UnitId",
+                        column: x => x.UnitId,
+                        principalTable: "Units",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Embeds_UnitId",
+                table: "Embeds",
+                column: "UnitId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Embeds");
+        }
+    }
+}
