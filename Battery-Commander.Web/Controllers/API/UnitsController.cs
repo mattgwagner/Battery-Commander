0438432b9cfe6a07cb08a7c81bfcbf2f1cbb48ed
mattgwagner <MattGWagner@gmail.com>
2018-08-28 21:47:55 -0400

    Stub endpoints for adding/removeing report settings

--- Battery-Commander.Web/Controllers/API/UnitsController.cs
+++ Battery-Commander.Web/Controllers/API/UnitsController.cs
@@ -1,9 +1,12 @@
 ﻿using BatteryCommander.Web.Models;
+using BatteryCommander.Web.Models.Settings;
+using BatteryCommander.Web.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using static BatteryCommander.Web.Models.Reports.Report;
 
 namespace BatteryCommander.Web.Controllers.API
 {
@@ -29,6 +32,50 @@ namespace BatteryCommander.Web.Controllers.API
                     unit.UIC
                 })
                 .ToListAsync();
+        }
+
+        [HttpGet("[controller]/{unitId}/reports")]
+        public async Task<IEnumerable<ReportSettings>> Reports(int unitId)
+        {
+            return
+                await db
+                .Units
+                .Where(unit => unit.Id == unitId)
+                .SelectMany(unit => unit.ReportSettings)
+                .ToListAsync();
+        }
+
+        [HttpPost("[controller]/{unitId}/reports")]
+        public async Task<IActionResult> AddReport([FromRoute]int unitId, [FromBody]ReportSettings settings)
+        {
+            var unit = await UnitService.Get(db, unitId);
+
+            unit.ReportSettings.Add(settings);
+
+            await db.SaveChangesAsync();
+
+            return Accepted();
+        }
+
+        [HttpDelete("[controller]/{unitId}/reports/{reportType}")]
+        public async Task<IActionResult> AddReport([FromRoute]int unitId, [FromRoute]ReportType type)
+        {
+            var unit = await UnitService.Get(db, unitId);
+
+            var to_remove =
+                unit
+                .ReportSettings
+                .Where(report => report.Type == type)
+                .FirstOrDefault();
+
+            if (to_remove != null)
+            {
+                unit.ReportSettings.Remove(to_remove);
+
+                await db.SaveChangesAsync();
+            }
+
+            return Ok();
         }
     }
 }
