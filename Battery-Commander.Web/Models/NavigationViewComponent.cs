6424c0b76df8e122e224fcfae58d36426812205f
Matt Wagner <mattgwagner@gmail.com>
2019-01-30 11:45:31 -0500

    Remove nav items
    For #359

--- Battery-Commander.Web/Models/NavigationViewComponent.cs
+++ Battery-Commander.Web/Models/NavigationViewComponent.cs
@@ -2,8 +2,6 @@
 using BatteryCommander.Web.Services;
 using Microsoft.AspNetCore.Mvc;
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace BatteryCommander.Web.Models
@@ -15,19 +13,6 @@ namespace BatteryCommander.Web.Models
         public Soldier Soldier { get; private set; }
 
         public Boolean ShowNavigation => !String.Equals(nameof(HomeController.PrivacyAct), RouteData.Values["action"]);
-
-        public IEnumerable<Embed> NavItems
-        {
-            get
-            {
-                // TODO Only for nav items
-                return
-                    db
-                    .Embeds
-                    .Where(embed => !embed.UnitId.HasValue || Soldier == null || embed.UnitId == Soldier.UnitId)
-                    .ToList();
-            }
-        }
 
         public NavigationViewComponent(Database db)
         {
