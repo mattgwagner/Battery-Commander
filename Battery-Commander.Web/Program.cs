32b99c1836ab3873c5d7cfb9dd4ce55f8ef01f70
mattgwagner <MattGWagner@gmail.com>
2019-04-27 21:44:06 -0400

    Tweak sentry usage

--- Battery-Commander.Web/Program.cs
+++ Battery-Commander.Web/Program.cs
@@ -14,6 +14,7 @@ namespace BatteryCommander.Web
                 .UseContentRoot(Directory.GetCurrentDirectory())
                 .UseIISIntegration()
                 .UseStartup<Startup>()
+                .UseSentry()
                 .UseSerilog((h, context) =>
                 {
                     context
