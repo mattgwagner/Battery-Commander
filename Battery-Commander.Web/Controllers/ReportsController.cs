d4d8ec053ac359f8c34a2ecddbc14a9c153be95c
mattgwagner <MattGWagner@gmail.com>
2019-06-01 10:31:32 -0400

    Initial work on report settings controller

--- Battery-Commander.Web/Controllers/ReportsController.cs
+++ Battery-Commander.Web/Controllers/ReportsController.cs
@@ -1,8 +1,10 @@
 ﻿using BatteryCommander.Web.Models;
 using BatteryCommander.Web.Models.Reports;
+using BatteryCommander.Web.Models.Settings;
 using BatteryCommander.Web.Services;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace BatteryCommander.Web.Controllers
@@ -15,6 +17,53 @@ namespace BatteryCommander.Web.Controllers
         public ReportsController(Database db)
         {
             this.db = db;
+        }
+
+        [Route("~/Units/{unitId}/Reports", Name = "Unit.Reports")]
+        public async Task<IActionResult> Index(int unitId)
+        {
+            return View(await UnitService.Get(db, unitId));
+        }
+
+        public async Task<IActionResult> Add(int unitId)
+        {
+            ViewBag.UnitId = unitId;
+
+            return View();
+        }
+
+        public async Task<IActionResult> Delete(int unitId, Report.ReportType type)
+        {
+            var unit = await UnitService.Get(db, unitId);
+
+            var settings =
+                unit
+                .ReportSettings
+                .SingleOrDefault(s => s.Type == type);
+
+            if (settings != null)
+            {
+                unit.ReportSettings.Remove(settings);
+
+                await db.SaveChangesAsync();
+            }
+
+            return RedirectToAction(nameof(Index), new { unitId });
+        }
+
+        public async Task<IActionResult> Save(int unitId, ReportSettings settings)
+        {
+            // Remove any if existing
+
+            await Delete(unitId, settings.Type);
+
+            var unit = await UnitService.Get(db, unitId);
+
+            unit.ReportSettings.Add(settings);
+
+            await db.SaveChangesAsync();
+
+            return RedirectToAction(nameof(Index), new { unitId });
         }
 
         // TAN 1 -- Comstat
