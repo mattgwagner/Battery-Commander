586b75935e8885220c772caac486a2074fcfa88c
mattgwagner <MattGWagner@gmail.com>
2019-06-01 10:06:32 -0400

    Remove embed from db
    Fixes #359

---
+++ Battery-Commander.Web/Migrations/20190601140618_RemoveEmbed.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BatteryCommander.Web.Migrations
+{
+    public partial class RemoveEmbed : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Embeds");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Embeds",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(maxLength: 20, nullable: true),
+                    Route = table.Column<string>(maxLength: 30, nullable: true),
+                    Source = table.Column<string>(nullable: true),
+                    UnitId = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Embeds", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Embeds_Units_UnitId",
+                        column: x => x.UnitId,
+                        principalTable: "Units",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Embeds_UnitId",
+                table: "Embeds",
+                column: "UnitId");
+        }
+    }
+}
