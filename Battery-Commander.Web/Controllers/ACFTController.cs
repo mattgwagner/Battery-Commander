c03155d32baf1799f3de560c1ee5f35d2fe61a2d
Matt Wagner <mattgwagner@gmail.com>
2019-09-02 23:22:05 -0400

    Add for record to edit

--- Battery-Commander.Web/Controllers/ACFTController.cs
+++ Battery-Commander.Web/Controllers/ACFTController.cs
@@ -2,8 +2,6 @@
 using BatteryCommander.Web.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
