fda215d32848d90461769a21bccf415f68e85447
Matt Wagner <mattgwagner@gmail.com>
2019-09-02 22:59:20 -0400

    Add acft controller and to nav

---
+++ Battery-Commander.Web/Controllers/ACFTController.cs
@@ -0,0 +1,24 @@
+﻿using BatteryCommander.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BatteryCommander.Web.Controllers
+{
+    public class ACFTController : Controller
+    {
+        private readonly Database db;
+
+        public ACFTController(Database db)
+        {
+            this.db = db;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return Content("Coming soon!");
+        }
+    }
+}
