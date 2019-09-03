7524a4c990a4867b0247a23cf470fff4cf93a0fb
Matt Wagner <mattgwagner@gmail.com>
2019-09-02 22:46:27 -0400

    Add data migration
    For #403

--- Battery-Commander.Web/Migrations/20190903024530_ACFT.Designer.cs
+++ Battery-Commander.Web/Migrations/20190903024530_ACFT.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using BatteryCommander.Web.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace BatteryCommander.Web.Migrations
 {
     [DbContext(typeof(Database))]
-    partial class DatabaseModelSnapshot : ModelSnapshot
+    [Migration("20190903024530_ACFT")]
+    partial class ACFT
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Battery-Commander.Web/Migrations/20190903024530_ACFT.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BatteryCommander.Web.Migrations
+{
+    public partial class ACFT : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ACFTs",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    SoldierId = table.Column<int>(nullable: false),
+                    Date = table.Column<DateTime>(type: "date", nullable: false),
+                    ForRecord = table.Column<bool>(nullable: false),
+                    ThreeRepMaximumDeadlifts = table.Column<int>(nullable: false),
+                    StandingPowerThrow = table.Column<int>(nullable: false),
+                    HandReleasePushups = table.Column<int>(nullable: false),
+                    SprintDragCarrySeconds = table.Column<int>(nullable: false),
+                    LegTucks = table.Column<int>(nullable: false),
+                    TwoMileRunSeconds = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ACFTs", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ACFTs_Soldiers_SoldierId",
+                        column: x => x.SoldierId,
+                        principalTable: "Soldiers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ACFTs_SoldierId",
+                table: "ACFTs",
+                column: "SoldierId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ACFTs");
+        }
+    }
+}
