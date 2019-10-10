cd54ed763a2d3d613a337da266bc7667db90ac70
Matt Wagner <mattgwagner@gmail.com>
2019-10-10 15:45:26 -0400

    Update Program.cs

--- Battery-Commander.Web/Program.cs
+++ Battery-Commander.Web/Program.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Hosting;
 using Sentry;
 using Serilog;
 using System.IO;
@@ -12,7 +12,6 @@ namespace BatteryCommander.Web
             using (SentrySdk.Init("https://78e464f7456f49a98e500e78b0bb4b13@sentry.io/1447369"))
             {
                 var host = new WebHostBuilder()
-                    .UseApplicationInsights()
                     .UseKestrel()
                     .UseContentRoot(Directory.GetCurrentDirectory())
                     .UseIISIntegration()
