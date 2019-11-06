929d9b0004a72b6889eada9ee05e18e4457df2a4
Matt Wagner <mattgwagner@gmail.com>
2019-11-06 11:20:04 -0500

    Refactor

--- Battery-Commander.Web/Startup.cs
+++ Battery-Commander.Web/Startup.cs
@@ -36,18 +36,13 @@ namespace BatteryCommander.Web
 
         private static Boolean IsDevelopment;
 
-        public Startup(IHostingEnvironment env)
+        public Startup(IHostingEnvironment env, IConfiguration configuration)
         {
-            var builder = new ConfigurationBuilder()
-                .SetBasePath(env.ContentRootPath)
-                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
-                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
-                .AddEnvironmentVariables();
-            Configuration = builder.Build();
             IsDevelopment = env.IsDevelopment();
+            Configuration = configuration;
         }
 
-        public IConfigurationRoot Configuration { get; }
+        public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
