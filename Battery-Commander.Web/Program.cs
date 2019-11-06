6518473697324d877e77cc12096f0c5f18ed8844
Matt Wagner <mattgwagner@gmail.com>
2019-11-06 11:18:34 -0500

    Clean-up startup

--- Battery-Commander.Web/Program.cs
+++ Battery-Commander.Web/Program.cs
@@ -1,7 +1,6 @@
+using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Sentry;
 using Serilog;
-using System.IO;
 
 namespace BatteryCommander.Web
 {
@@ -9,23 +8,20 @@ namespace BatteryCommander.Web
     {
         public static void Main(string[] args)
         {
-            using (SentrySdk.Init("https://78e464f7456f49a98e500e78b0bb4b13@sentry.io/1447369"))
-            {
-                var host = new WebHostBuilder()
-                    .UseKestrel()
-                    .UseContentRoot(Directory.GetCurrentDirectory())
-                    .UseIISIntegration()
-                    .UseStartup<Startup>()
-                    .UseSerilog((h, context) =>
-                    {
-                        context
-                        .Enrich.FromLogContext()
-                        .WriteTo.Sentry();
-                    })
-                    .Build();
+            var host =
+                WebHost
+                .CreateDefaultBuilder(args)
+                .UseSerilog((h, context) =>
+                {
+                    context
+                    .Enrich.FromLogContext()
+                    .WriteTo.Sentry();
+                })
+                .UseSentry(dsn: "https://78e464f7456f49a98e500e78b0bb4b13@sentry.io/1447369")
+                .UseStartup<Startup>()
+                .Build();
 
-                host.Run();
-            }
+            host.Run();
         }
     }
 }
