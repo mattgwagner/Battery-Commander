520bc3943fefbb1802463623400adad89dd72e78
Matt Wagner <mattgwagner@gmail.com>
2019-11-22 16:11:26 -0500

    Migration to support vehicle update date
    For #447

--- Battery-Commander.Web/Migrations/20191122211043_VehicleUpdateDate.Designer.cs
+++ Battery-Commander.Web/Migrations/20191122211043_VehicleUpdateDate.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using BatteryCommander.Web.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace BatteryCommander.Web.Migrations
 {
     [DbContext(typeof(Database))]
-    partial class DatabaseModelSnapshot : ModelSnapshot
+    [Migration("20191122211043_VehicleUpdateDate")]
+    partial class VehicleUpdateDate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
