ecc493865ab69d55568306116e41039902a367f6
Matt Wagner <mattgwagner@gmail.com>
2020-02-02 13:29:55 -0500

    Move perstat update to reports controller

--- Battery-Commander.Web/Controllers/ReportsController.cs
+++ Battery-Commander.Web/Controllers/ReportsController.cs
@@ -4,7 +4,9 @@ using BatteryCommander.Web.Models.Settings;
 using BatteryCommander.Web.Services;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -134,6 +136,68 @@ namespace BatteryCommander.Web.Controllers
             });
 
             return View(soldiers);
+        }
+
+        // List status for all soldiers by - filter by unit
+
+        // Bulk update statuses
+
+        [HttpGet("~/Units/{Unit}/PERSTAT")]
+        public async Task<IActionResult> PERSTAT(int Unit)
+        {
+            return View("List", new StatusListModel
+            {
+                Unit = Unit,
+                Rows =
+                    (await SoldierService.Filter(db, new SoldierService.Query
+                    {
+                        Unit = Unit
+                    }))
+                    .Select(soldier => new StatusListModel.Row
+                    {
+                        Soldier = soldier,
+                        SoldierId = soldier.Id,
+                        Status = soldier.Status
+                    })
+                    .ToList()
+            });
+        }
+
+        [HttpPost("~/Unis/{Unit}/PERSTAT"), ValidateAntiForgeryToken]
+        public async Task<IActionResult> PERSTAT(int Unit, StatusListModel model)
+        {
+            // For each DTO posted, update the soldier info
+
+            foreach (var dto in model.Rows)
+            {
+                var soldier =
+                    await db
+                    .Soldiers
+                    .Where(_ => _.Id == dto.SoldierId)
+                    .SingleOrDefaultAsync();
+
+                soldier.Status = dto.Status;
+            }
+
+            await db.SaveChangesAsync();
+
+            return RedirectToAction(nameof(SoldiersController.ForUnit), "Soldiers", new { unitId = Unit });
+        }
+
+        public class StatusListModel
+        {
+            public IList<Row> Rows { get; set; }
+
+            public int Unit { get; set; }
+
+            public class Row
+            {
+                public Soldier Soldier { get; set; }
+
+                public int SoldierId { get; set; }
+
+                public Soldier.SoldierStatus Status { get; set; } = Soldier.SoldierStatus.Unknown;
+            }
         }
     }
 }
