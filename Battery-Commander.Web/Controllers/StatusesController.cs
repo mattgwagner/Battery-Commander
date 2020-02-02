c84d12a9fdd7609accaff62454aaeca1dc92c565
Matt Wagner <mattgwagner@gmail.com>
2020-02-02 12:58:12 -0500

    Removed unused imports

--- Battery-Commander.Web/Controllers/StatusesController.cs
+++ Battery-Commander.Web/Controllers/StatusesController.cs
@@ -1,9 +1,7 @@
 ﻿using BatteryCommander.Web.Models;
 using BatteryCommander.Web.Services;
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
