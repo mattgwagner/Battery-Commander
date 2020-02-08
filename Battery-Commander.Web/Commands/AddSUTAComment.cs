48b3196865e4a4d8da5f59de6ab4a33d2ea9236c
Matt Wagner <mattgwagner@gmail.com>
2020-02-07 20:39:35 -0500

    Fix add signature and comment

---
+++ Battery-Commander.Web/Commands/AddSUTAComment.cs
@@ -0,0 +1,51 @@
+﻿using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using BatteryCommander.Web.Models;
+using BatteryCommander.Web.Queries;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BatteryCommander.Web.Commands
+{
+    public class AddSUTAComment : IRequest
+    {
+        [FromRoute]
+        public int Id { get; set; }
+
+        [FromForm]
+        public string Comment { get; set; }
+
+        private class Handler : AsyncRequestHandler<AddSUTAComment>
+        {
+            private readonly Database db;
+            private readonly IMediator dispatcher;
+
+            public Handler(Database db, IMediator dispatcher)
+            {
+                this.db = db;
+                this.dispatcher = dispatcher;
+            }
+
+            protected override async Task Handle(AddSUTAComment request, CancellationToken cancellationToken)
+            {
+                var suta =
+                    await db
+                    .SUTAs
+                    .Where(s => s.Id == request.Id)
+                    .SingleOrDefaultAsync(cancellationToken);
+
+                var current_user = await dispatcher.Send(new GetCurrentUser { });
+
+                suta.Events.Add(new SUTA.Event
+                {
+                    Author = $"{current_user}",
+                    Message = request.Comment
+                });
+
+                await db.SaveChangesAsync(cancellationToken);
+            }
+        }
+    }
+}
