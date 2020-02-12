20bd26722cc59d565255637791e86ac81becba6b
Matt Wagner <mattgwagner@gmail.com>
2020-02-12 10:56:19 -0500

    DTStamp tweak

--- Battery-Commander.Web/Services/CalendarService.cs
+++ Battery-Commander.Web/Services/CalendarService.cs
@@ -115,7 +115,7 @@ namespace BatteryCommander.Web.Services
             return new StringBuilder()
                 .AppendLine("BEGIN:VEVENT")
                 .AppendLine($"SUMMARY:{entry.Description}")
-                .AppendLine($"DTSTAMP:{entry.Date.ToString("yyyyMMdd")}")
+                .AppendLine($"DTSTAMP:{DateTime.UtcNow.ToUniversalTime().ToString(DateFormat)}")
                 .AppendLine("UID:" + (entry.Description + entry.Date).GetHashCode())
                 .AppendLine("DTSTART:" + entry.Date.ToString("yyyyMMdd"))
                 .AppendLine("LAST-MODIFIED:" + DateTime.UtcNow.ToUniversalTime().ToString(DateFormat))
