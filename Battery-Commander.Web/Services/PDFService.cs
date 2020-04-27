9da4e7b20c063b264f126b13142bb5c014ee68cd
Matt Wagner <mattgwagner@gmail.com>
2020-04-26 23:13:01 -0400

    Remove old reference

--- Battery-Commander.Web/Services/PDFService.cs
+++ Battery-Commander.Web/Services/PDFService.cs
@@ -17,7 +17,7 @@ namespace BatteryCommander.Web.Services
     {
         private static readonly HttpClient http = new HttpClient { };
 
-        private static RedLeg_FormsClient forms_client => new RedLeg_FormsClient("https://forms.redleg.app", http);
+        private static Forms.RedLeg_FormsClient forms_client => new Forms.RedLeg_FormsClient("https://forms.redleg.app", http);
 
         static PDFService()
         {
@@ -49,48 +49,6 @@ namespace BatteryCommander.Web.Services
             await result.Stream.CopyToAsync(memory);
 
             return memory.ToArray();
-        }
-
-        [Obsolete("Migrate to using RedLeg.Forms")]
-        public static byte[] Generate_DA4856(Counseling model)
-        {
-            const String prefix = "form1[0]";
-
-            using (var stream = typeof(Program).GetTypeInfo().Assembly.GetManifestResourceStream("Battery-Commander.Web.Models.Data.DA4856.pdf"))
-            using (var output = new MemoryStream())
-            {
-                var reader = new PdfReader(stream);
-                var stamper = new PdfStamper(reader, output);
-
-                var form = stamper.AcroFields;
-
-                //form1[0].Page1[0].Name_Title_Counselor[0]
-                //form1[0].Page1[0].Key_Points_Disscussion[0]
-                //form1[0].Page1[0].Name[0]
-                //form1[0].Page1[0].Date_Counseling[0]
-                //form1[0].Page1[0].Rank_Grade[0]
-                //form1[0].Page2[0].Leader_Responsibilities[0]
-                //form1[0].Page1[0].Purpose_Counseling[0]
-                //form1[0].Page1[0].Organization[0]
-                //form1[0].Page2[0].Plan_Action[0]
-
-                // Update the form fields as appropriate
-
-                form.SetField($"{prefix}.Page1[0].Name[0]", model.Name);
-                form.SetField($"{prefix}.Page1[0].Name_Title_Counselor[0]", model.Counselor);
-                form.SetField($"{prefix}.Page1[0].Key_Points_Disscussion[0]", model.KeyPointsOfDiscussion);
-                form.SetField($"{prefix}.Page1[0].Date_Counseling[0]", model.Date.ToString("yyyy-MM-dd"));
-                form.SetField($"{prefix}.Page1[0].Rank_Grade[0]", model.Rank.DisplayName());
-                form.SetField($"{prefix}.Page2[0].Leader_Responsibilities[0]", model.LeadersResponsibilities);
-                form.SetField($"{prefix}.Page1[0].Purpose_Counseling[0]", model.Purpose);
-                form.SetField($"{prefix}.Page1[0].Organization[0]", model.Organization);
-                form.SetField($"{prefix}.Page2[0].Plan_Action[0]", model.PlanOfAction);
-                form.SetField($"{prefix}.Page2[0].Assessment[0]", model.Assessment);
-
-                stamper.Close();
-
-                return output.ToArray();
-            }
         }
 
         public static byte[] Generate_DA5500(ABCP model)
