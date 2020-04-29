5b5e95b69af18a8f22b975fb409e0937f7b70247
Matt Wagner <mattgwagner@gmail.com>
2020-04-29 15:01:10 -0400

    Add basic test for pushups and run

--- Battery-Commander.Tests/ACFTTests.cs
+++ Battery-Commander.Tests/ACFTTests.cs
@@ -8,9 +8,21 @@ namespace BatteryCommander.Tests
     {
         [Theory]
         [InlineData(15, 54, 84)]
+        [InlineData(13, 29, 100)]
+        [InlineData(23, 01, 0)]
+        [InlineData(19, 05, 64)]
+        [InlineData(17, 42, 72)]
         public void Calculate_Run_Score(int minutes, int seconds, int expected)
         {
             Assert.Equal(expected, ACFTScoreTables.TwoMileRun(new TimeSpan(0, minutes, seconds)));
+        }
+
+        [Theory]
+        [InlineData(61, 100)]
+        [InlineData(30, 70)]
+        public void Calculate_Pushups(int reps, int expected)
+        {
+            Assert.Equal(expected, ACFTScoreTables.HandReleasePushUps(reps));
         }
     }
 }
