469450075c66a9f96690abcb19d21d7be268138b
Matt Wagner <Matt@Red-Leg-Dev.com>
2020-04-30 13:22:34 -0400

    Update Program.cs

--- Battery-Commander.Web/Program.cs
+++ Battery-Commander.Web/Program.cs
@@ -31,7 +31,7 @@ namespace BatteryCommander.Web
                            .Enrich.WithProperty("Application", context.HostingEnvironment.ApplicationName)
                            .Enrich.WithProperty("Environment", context.HostingEnvironment.EnvironmentName)
                            .Enrich.WithProperty("Version", $"{typeof(Startup).Assembly.GetName().Version}")
-                           .WriteTo.Seq(serverUrl: "http://redlegdev-logs.eastus.azurecontainer.io", apiKey: context.Configuration.GetValue<string>("Seq:ApiKey"), compact: true, controlLevelSwitch: LogLevel)
+                           .WriteTo.Seq(serverUrl: "https://logs.redleg.app", apiKey: context.Configuration.GetValue<string>("Seq:ApiKey"), compact: true, controlLevelSwitch: LogLevel)
                            .MinimumLevel.ControlledBy(LogLevel)
                            .CreateLogger();
 
