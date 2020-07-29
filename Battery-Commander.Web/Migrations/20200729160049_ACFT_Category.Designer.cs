4c1c3cc3504641809235fb3a67e75d54398c072b
Matt Wagner <mattgwagner@gmail.com>
2020-07-29 12:01:13 -0400

    Add DB properties for ACFT

--- Battery-Commander.Web/Migrations/20200729160049_ACFT_Category.Designer.cs
+++ Battery-Commander.Web/Migrations/20200729160049_ACFT_Category.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using BatteryCommander.Web.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace BatteryCommander.Web.Migrations
 {
     [DbContext(typeof(Database))]
-    partial class DatabaseModelSnapshot : ModelSnapshot
+    [Migration("20200729160049_ACFT_Category")]
+    partial class ACFT_Category
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Battery-Commander.Web/Migrations/20200729160049_ACFT_Category.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BatteryCommander.Web.Migrations
+{
+    public partial class ACFT_Category : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<byte>(
+                name: "ACFT_Grading_Standard",
+                table: "Soldiers",
+                nullable: false,
+                defaultValue: (byte)0);
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "StandingPowerThrow",
+                table: "ACFTs",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "INTEGER");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ACFT_Grading_Standard",
+                table: "Soldiers");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "StandingPowerThrow",
+                table: "ACFTs",
+                type: "INTEGER",
+                nullable: false,
+                oldClrType: typeof(decimal));
+        }
+    }
+}
