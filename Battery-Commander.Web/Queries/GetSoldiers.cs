b5b3cb9e4d23c00d1b4fe9b6bd063fabd1e7121d
Matt Wagner <mattgwagner@gmail.com>
2020-07-31 10:03:09 -0400

    Pulled over query filters into get soldiers

--- Battery-Commander.Web/Queries/GetSoldiers.cs
+++ Battery-Commander.Web/Queries/GetSoldiers.cs
@@ -1,4 +1,6 @@
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using BatteryCommander.Web.Models;
@@ -9,7 +11,35 @@ namespace BatteryCommander.Web.Queries
 {
     public class GetSoldiers : IRequest<IEnumerable<Soldier>>
     {
-        // TODO Port over the filters from SoldierService
+        public int? Id { get; set; }
+
+        public int[] Ids { get; set; }
+
+        public int? Unit { get; set; }
+
+        public IEnumerable<Rank> Ranks { get; set; }
+
+        public Boolean? OnlyEnlisted { get; set; }
+
+        public EventStatus? ABCP { get; set; }
+
+        public EventStatus? APFT { get; set; }
+
+        public Boolean? DSCA { get; set; }
+
+        public Boolean? IWQ { get; set; }
+
+        public Gender? Gender { get; set; }
+
+        public Boolean? SSD { get; set; }
+
+        public Boolean? CLS { get; set; }
+
+        public Boolean? EducationComplete { get; set; }
+
+        public Soldier.SoldierStatus? Status { get; set; }
+
+        public String Email { get; set; }
 
         private class Handler : IRequestHandler<GetSoldiers, IEnumerable<Soldier>>
         {
@@ -20,7 +50,7 @@ namespace BatteryCommander.Web.Queries
                 this.db = db;
             }
 
-            public async Task<IEnumerable<Soldier>> Handle(GetSoldiers request, CancellationToken cancellationToken)
+            public async Task<IEnumerable<Soldier>> Handle(GetSoldiers query, CancellationToken cancellationToken)
             {
                 var soldiers =
                     db
@@ -32,6 +62,88 @@ namespace BatteryCommander.Web.Queries
                     .Include(s => s.APFTs)
                     .Include(s => s.Unit)
                     .AsNoTracking();
+
+                if (query.Id.HasValue)
+                {
+                    soldiers = soldiers.Where(_ => _.Id == query.Id);
+                }
+
+                if (query.Ids?.Any() == true)
+                {
+                    soldiers = soldiers.Where(_ => query.Ids.Contains(_.Id));
+                }
+
+                if (query.Unit.HasValue)
+                {
+                    soldiers = soldiers.Where(_ => _.UnitId == query.Unit);
+                }
+
+                if (query.OnlyEnlisted == true)
+                {
+                    query.Ranks = RankExtensions.Enlisted;
+                }
+
+                if (query.Ranks?.Any() == true)
+                {
+                    soldiers = soldiers.Where(_ => query.Ranks.Contains(_.Rank));
+                }
+
+                if (query.ABCP.HasValue)
+                {
+                    soldiers = soldiers.Where(_ => _.AbcpStatus == query.ABCP);
+                }
+
+                if (query.APFT.HasValue)
+                {
+                    soldiers = soldiers.Where(_ => _.ApftStatus == query.APFT);
+                }
+
+                if (query.SSD.HasValue)
+                {
+                    if (query.SSD == true)
+                    {
+                        soldiers = soldiers.Where(_ => _.SSDStatus.CurrentProgress >= Decimal.One);
+                    }
+                    else
+                    {
+                        soldiers = soldiers.Where(_ => _.SSDStatus.CurrentProgress < Decimal.One);
+                    }
+                }
+
+                if (query.DSCA.HasValue)
+                {
+                    soldiers = soldiers.Where(_ => _.DscaQualified == query.DSCA);
+                }
+
+                if (query.Gender.HasValue)
+                {
+                    soldiers = soldiers.Where(_ => _.Gender == query.Gender);
+                }
+
+                if (query.IWQ.HasValue)
+                {
+                    soldiers = soldiers.Where(_ => _.IwqQualified == query.IWQ);
+                }
+
+                if (query.EducationComplete.HasValue)
+                {
+                    soldiers = soldiers.Where(_ => _.IsEducationComplete == query.EducationComplete);
+                }
+
+                if (query.CLS.HasValue)
+                {
+                    soldiers = soldiers.Where(_ => _.ClsQualified == query.CLS);
+                }
+
+                if (query.Status.HasValue)
+                {
+                    soldiers = soldiers.Where(_ => _.Status == query.Status);
+                }
+
+                if (!String.IsNullOrWhiteSpace(query.Email))
+                {
+                    soldiers = soldiers.Where(soldier => soldier.CivilianEmail.ToUpper() == query.Email.ToUpper() || soldier.MilitaryEmail.ToUpper() == query.Email.ToUpper());
+                }
 
                 return soldiers;
             }
