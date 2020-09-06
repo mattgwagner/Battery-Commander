14ed54ca04a9071478c92b3c2881f5ab82d441db
Matt Wagner <mattgwagner@gmail.com>
2020-09-06 17:09:41 -0400

    Add migration for archived

--- Battery-Commander.Web/Migrations/20200906210921_SUTA_Archived.Designer.cs
+++ Battery-Commander.Web/Migrations/20200906210921_SUTA_Archived.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using BatteryCommander.Web.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace BatteryCommander.Web.Migrations
 {
     [DbContext(typeof(Database))]
-    partial class DatabaseModelSnapshot : ModelSnapshot
+    [Migration("20200906210921_SUTA_Archived")]
+    partial class SUTA_Archived
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Battery-Commander.Web/Migrations/20200906210921_SUTA_Archived.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BatteryCommander.Web.Migrations
+{
+    public partial class SUTA_Archived : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "Archived",
+                table: "SUTAs",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Archived",
+                table: "SUTAs");
+        }
+    }
+}
